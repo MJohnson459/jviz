782abf83eaf6a08da3be764934cb67f9f4a0e049
michael.johnson <michael.johnson@seebyte.com>
2017-04-10 14:03:05 +0100

    Added list of messages to subscriber

--- src/Subscriber.js
+++ src/Subscriber.js
@@ -5,6 +5,15 @@ import Widget from './Widget.js';
 import YAML from 'yamljs';
 import { Scrollbars } from 'react-custom-scrollbars';
 import './styles/dark.css';
+import {List, AutoSizer} from 'react-virtualized'
+
+function Message(props) {
+    return (
+        <SyntaxHighlighter language="yaml" className={"Message"} useInlineStyles={false}>
+            {YAML.stringify(props.message, 2)}
+        </SyntaxHighlighter>
+    )
+}
 
 class Subscriber extends Component {
 
@@ -18,6 +27,7 @@ class Subscriber extends Component {
         }
 
         this.subscribe();
+        this.rowRenderer = this.rowRenderer.bind(this);
     }
 
     // componentWillUnmount() {
@@ -25,46 +35,58 @@ class Subscriber extends Component {
     // }
 
     subscribe() {
-      console.log("this.props: ", this.props)
-      this.subscriber = new ROSLIB.Topic({
-          ros : this.props.ros,
-          name : this.props.topic,
-          messageType : this.props.type,
-      });
+        console.log("this.props: ", this.props)
+        this.subscriber = new ROSLIB.Topic({
+            ros : this.props.ros,
+            name : this.props.topic,
+            messageType : this.props.type,
+        });
 
-      this.subscriber.subscribe((message) => {
-          this.setState(prevState => ({
-            // messages: [...prevState.messages, message],
-            message: message,
+        this.subscriber.subscribe((message) => {
+            this.setState(prevState => ({
+                messages: [...prevState.messages, message],
+                message: message,
           }));
-      });
+        });
+    }
 
+    rowRenderer({
+      key,         // Unique key within array of rows
+      index,       // Index of row within collection
+      isScrolling, // The List is currently being scrolled
+      isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+      style        // Style object to be applied to row (to position it)
+    }) {
+        const message = this.state.messages[index];
+
+        return (
+            <div
+              key={key}
+              style={style}
+            >
+                <Message message={message}/>
+            </div>)
     }
 
     render() {
         console.log('Rendering Subscriber');
 
-        // console.log("Subscriber render: ", this.state.messages);
-
-        // <ul>
-        // {
-        //     this.state.messages.map((message, i) =>
-        //         <li key={i}>
-        //             <SyntaxHighlighter language="yaml" style={docco}>
-        //                 {YAML.stringify(message, 2)}
-        //             </SyntaxHighlighter>
-        //         </li>
-        //     )
-        // }
-        // </ul>
-
         return (
         <Widget {...this.props} name={this.props.topic}>
-            <Scrollbars className={"Subscriber"} style={{ height: "inherit" }}>
-                <SyntaxHighlighter language="yaml" className={"Message"} useInlineStyles={false}>
-                    {YAML.stringify(this.state.message, 2)}
-                </SyntaxHighlighter>
-            </Scrollbars>
+            <div>{this.state.messages.length}</div>
+            <div style={{ flex: '1 1 auto' }}>
+            <AutoSizer>
+              {({ height, width }) => (
+                <List
+                    height={height}
+                    rowHeight={60}
+                    rowCount={this.state.messages.length}
+                    rowRenderer={this.rowRenderer}
+                    width={width}
+                />
+              )}
+            </AutoSizer>
+            </div>
             {this.props.children}
         </Widget>
         );
