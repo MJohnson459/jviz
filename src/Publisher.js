b8f2846458f8d4a7e6c237f72aff7cbf7dbc7339
MJohnson459 <mjohnson459@gmail.com>
2017-05-02 00:01:45 +0100

    Started decoding messages into a dictionary

--- src/Publisher.js
+++ src/Publisher.js
@@ -19,14 +19,60 @@ class Publisher extends Component {
         });
 
         this.props.ros.getMessageDetails(this.props.type, (details)=>{
+
+
             this.setState({
                 messageDetails: details,
+                messageDef: this.decodeTypeDefsRec(details[0], details),
             })
         }, (message)=>{
             console.log("msg details FAILED", this.props.type, message)
         });
 
         this.publish = this.publish.bind(this);
+        this.decodeTypeDefsRec = this.decodeTypeDefsRec.bind(this);
+    }
+
+    // calls itself recursively to resolve type definition using hints.
+    decodeTypeDefsRec(theType, hints) {
+      var typeDefDict = {};
+      for (var i = 0; i < theType.fieldnames.length; i++) {
+        const arrayLen = theType.fieldarraylen[i];
+        const fieldName = theType.fieldnames[i];
+        const fieldType = theType.fieldtypes[i];
+        const fieldExample = theType.examples[i];
+        if (fieldType.indexOf('/') === -1) { // check the fieldType includes '/' or not
+          if (arrayLen === -1) {
+            typeDefDict[fieldName] = {type: fieldType, example: fieldExample};
+          }
+          else {
+            typeDefDict[fieldName] = [fieldType];
+          }
+        }
+        else {
+          // lookup the name
+          var sub = false;
+          for (var j = 0; j < hints.length; j++) {
+            if (hints[j].type.toString() === fieldType.toString()) {
+              sub = hints[j];
+              break;
+            }
+          }
+          if (sub) {
+            const subResult = this.decodeTypeDefsRec(sub, hints);
+            if (arrayLen === -1) {
+              typeDefDict[fieldName] = subResult;
+            }
+            else {
+              typeDefDict[fieldName] = [subResult];
+            }
+          }
+          else {
+            console.log('error', 'Cannot find ' + fieldType + ' in decodeTypeDefs');
+          }
+        }
+      }
+      return typeDefDict;
     }
 
     publish() {
@@ -46,7 +92,7 @@ class Publisher extends Component {
                 { this.state.messageDetails === null ||
                 <div style={{display: "flex", flexDirection: "column", flex: 1}}>
                       <div style={{padding: 5, overflowY: "auto", flex: 1}}>
-                        <Message name={this.props.type} messages={this.state.messageDetails} index={0} indent={0} />
+                        <Message name={this.props.type} messages={this.state.messageDetails} />
                       </div>
                       <div style={{display: "flex", flex: "0 0 25px", flexDirection: "row"}}>
                         <div className="SmallButton ColorOne" onClick={this.publish}>
