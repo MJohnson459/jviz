6b67cdbdb1bc41534353866636297e78b9ffda46
MJohnson459 <mjohnson459@gmail.com>
2017-07-05 23:52:27 +0100

    Attempted to add storybooks

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ROSLIB from 'roslib';
+import {Treebeard} from 'react-treebeard';
 
 import { storiesOf } from '@storybook/react';
 // import { action } from '@storybook/addon-actions';
@@ -6,27 +8,78 @@ import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
 
 import NodeTree from '../NodeTree';
+import RosGraph from '../RosGraph';
+import NodeGraph from '../NodeGraph';
+import Widget from '../Widget';
+
+import styles from '../styles/treebeard-theme';
+import '../App.css';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('NodeTree', module)
+class MockTopicList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tree: [],
+    }
+
+    const ros = new ROSLIB.Ros({
+      url : "ws://localhost:9090",
+    });
+
+
+    ros.on('connection', () => {
+      console.log('Connected to websocket server.');
+
+      RosGraph.getRosGraph(ros).then((graph) => {
+        console.table(graph)
+        this.setState({
+          tree: NodeTree.getNodeTree(_.filter(graph, {type: "node"})),
+        });
+      });
+    });
+  }
+
+  render() {
+    return (<Treebeard
+        data={this.state.tree}
+        onToggle={() => console.log("toggled")}
+        style={styles}
+     />)
+  }
+}
+
+storiesOf('RosGraph', module)
+  .add('basic', () => <MockTopicList />);
+
+function connectRos(ros) {
+  return new Promise((resolve) => {
+    ros.on('connection', () => {
+      return resolve()
+    })
+  })
+}
+
+storiesOf('NodeGraph', module)
   .add('basic', () => {
+    const ros = new ROSLIB.Ros({
+      url : "ws://localhost:9090",
+    });
 
-    const nodes = [
-      {
-        details: {},
-        id: "n_/listener_one",
-        name: "/listener_one/t1/t3",
-        selected: false,
-      },{
-        details: {},
-        id: "n_/listener_two",
-        name: "/listener_two/t5/t1",
-        selected: false,
-      },
-    ];
+    const promise = connectRos(ros)
+      .then(RosGraph.getRosGraph(ros))
+      .then((graph) => {
+        return NodeTree.getNodeTree(_.filter(graph, {type: "node"}));
+      });
 
-    return (
-      <NodeTree nodes={nodes} />
+    console.table(promise);
+
+    return(
+      <Widget key={"Node_Graph"} name={"Node Graph"} onRequestClose={() => console.log("Remove Node Graph")}>
+        <NodeGraph nodeList={promise.state === "fulfilled" ? value : []} />
+      </Widget>
     )
   });
