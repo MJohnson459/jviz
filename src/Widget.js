20f7420d5728523f83c0c4a488823d4e8180e598
MJohnson459 <mjohnson459@gmail.com>
2017-12-14 23:26:57 +0000

    Minor aesthetic help

--- src/Widget.js
+++ src/Widget.js
@@ -16,11 +16,17 @@ class Widget extends React.Component<Props, State> {
     minimised: false
   }
 
+  formatName = (name: string) => {
+    const maxlen = 25
+    if (name.length > maxlen) return name.slice(0, maxlen-3) + "..."
+    return name
+  }
+
   render() {
     return (
      <div className={'Widget'} >
        <div className={'Header'} onClick={() => this.setState({minimised: !this.state.minimised})}>
-        <div className={'Name'}>{this.props.name}</div>
+        <div className={'Name'}>{this.formatName(this.props.name)}</div>
         {this.props.onRequestClose ? <div className={'Close'} onClick={this.props.onRequestClose}>x</div> : false}
       </div>
       {this.state.minimised ? false : <div className={'Main'}>{this.props.children}</div>}
